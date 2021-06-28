db6643da9e99c8e675e0041413cba2f237611f1f
Ajith2112 <86114872+Ajith2112@users.noreply.github.com>
2021-06-28 22:55:47 +0530

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,39 @@
+
+
+function validate (){
+    var box1=document.getElementById("name");
+    if(box1.value==""){
+        document.getElementById("p1").innerHTML="*valid*";
+        box1.focus;
+        box1.style.border="solid 5px red"
+        return false;
+        
+    }
+    if(box1.value.length>10){
+        document.getElementById("p1").innerHTML="*Name lenght is too large";
+        box1.focus;
+        box1.style.border="solid 5px red"
+        return true;
+        
+    }
+
+    var box2=document.getElementById("phone");
+    if(box2.value==""){
+        document.getElementById("p2").innerHTML="*Enter a Valid Number";
+        box2.focus;
+        box2.style.border="solid 5px red"
+        return false;
+        
+    }
+    if(box2.value.length>10){
+        document.getElementById("p2").innerHTML="*Enter a Valid Number";
+        box2.focus;
+        box2.style.border="solid 5px red"
+        return false;
+        
+    }
+     
+    
+
+    
+}
